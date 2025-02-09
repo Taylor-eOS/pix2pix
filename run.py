cd7c90d73cf26d834dd978e51a7ed8167ef09601
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-02-09 20:50:15 +0100

    Save image

--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDisc
 
 model_id = "timbrooks/instruct-pix2pix"
 pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = pipe.to(device)
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
 image_path = "example.jpg"
 image = PIL.Image.open(image_path)
 image = PIL.ImageOps.exif_transpose(image)
 image = image.convert("RGB")
+prompt = input("Prompt: ")
+images = pipe(prompt, image=image, num_inference_steps=5, image_guidance_scale=1).images
 
-prompt = "make him look like he was a alive man"
-images = pipe(prompt, image=image, num_inference_steps=10, image_guidance_scale=1).images
-images[0]
+output_path = "output_image.jpg"
+images[0].save(output_path)
+print(f"Image saved to {output_path}")
+
